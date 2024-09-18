427f1d7065458998db01c186ce3b54d7c03c48ee
GooGaru <xwilliam.morrisonx@gmail.com>
2024-09-18 17:01:46 -0400

    Localstorage is working, getting alot of chrome extentions errors though

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,22 +1,21 @@
-const usernameEl = document.getElementById("username");
-const titleEl = document.getElementById("title");
-const contentEl = document.getElementById("content");
+const usernameEl = document.getElementById('username');
+const titleEl = document.getElementById('title');
+const contentEl = document.getElementById('content');
+const submitBtn = document.getElementById('submitBtn')
+const formEl = document.getElementById('blogPost');
 
-const formEl = document.getElementById("blogPost");
-
-formEl.addEventListener("submit", (event) => {
+formEl.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log("do i work");
 
-  // const userNameValue = userNameEl.value;
-  // const titleElValue = titleEl.value;
-  // const contentElValue = contentEl.value;
+  const userNameValue = usernameEl.value;
+  const titleElValue = titleEl.value;
+  const contentElValue = contentEl.value;
 
-  // const blogData = {
-  //     userName: userNameValue,
-  //     title: titleElValue,
-  //     content: contentElValue
-  // }
+  const blogData = {
+      userName: userNameValue,
+      title: titleElValue,
+      content: contentElValue
+  }
 
-  // localStorage.setItem('blog', JSON.stringify(blogData));
+  localStorage.setItem('blog', JSON.stringify(blogData));
 });
